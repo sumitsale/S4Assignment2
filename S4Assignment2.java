6cce3cac7ab598d792e813c7405a7f738efcf036
[Sumit Sale] <[sumit.sale@citiustech.com]>
2016-04-18 12:46:42 +0530

    Java Session 4 Assignment 2

---
+++ S4Assignment2.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class S4Assignment2
+{
+   public static void main(String args[])
+   {
+	   /*
+		int[] firstArray = new int[]{11,232,3,44};
+		
+		System.out.println("Original Array");
+		
+		for(int a : firstArray) {
+		
+			System.out.println(a);
+		}
+		
+		System.out.println("");
+		
+		System.out.println("Sorted Array");
+		
+		Arrays.sort(firstArray);
+		
+		for(int a : firstArray) {
+		
+			System.out.println(a);
+		}
+	   */	
+
+
+		int n, pos, x;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n+1];
+        System.out.println("Enter all the elements:");
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        System.out.print("Enter the position where you want to insert element:");
+        pos = s.nextInt();
+        System.out.print("Enter the element you want to insert:");
+        x = s.nextInt();
+        for(int i = (n-1); i >= (pos-1); i--)
+        {
+            a[i+1] = a[i];
+        }
+        a[pos-1] = x;
+        System.out.print("After inserting:");
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(a[i]+",");
+        }
+        System.out.print(a[n]);
+		
+		
+		
+		System.out.println("Original Array");
+		
+		for(int i : a) {
+		
+			System.out.println(i);
+		}
+		
+		System.out.println("");
+		
+		System.out.println("Sorted Array");
+		
+		Arrays.sort(a);
+		
+		for(int i : a) {
+		
+			System.out.println(i);
+		}
+   }
+}
